e4b3185e398712cafa6777d9d89e413edaaf4ea0
Loisteg <loisteg@outlook.com>
2022-04-26 18:20:54 +0300

    Added info section

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,46 @@
-import './_vendor';
-import './_functions';
-import './_components';
+import "./_vendor";
+import "./_functions";
+import "./_components";
 
-import $ from 'jquery';
+import $ from "jquery";
 
-$('#form').on('submit', function (e) {
+$("#form").on("submit", function (e) {
   e.preventDefault();
 
-  const name = this.name;
-  const phone = this.phone;
-  const tgName = this.tg_name;
+  const TOKEN = "";
+  const CHAT_ID = "";
+  const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+  let name = this.name.value;
+  let phone = this.phone.value;
+  let tgName = this.tg_name.value;
+
+  const message = `
+  <b>ФИ: ${name}</b>
+  <b>Номер телефона: ${phone}</b>
+  <b>Номер телефона: @${tgName}</b>`;
 
   // TODO: Вставить нужный url для отправки
-  const request = $.ajax('', {
-    type: 'POST',
-    data: {},
+  const request = $.ajax(URI_API, {
+    type: "POST",
+    data: {
+      chat_id: CHAT_ID,
+      parse_mode: "html",
+      text: message,
+    },
   });
 
   // выполняется после *успешного выполнения запроса
-  request.done(function () {});
+  request.done(function () {
+    name = "";
+    phone = "";
+    tgName = "";
+  });
   // выполняется после *провального выполнения запроса
-  request.fail(function () {});
+  request.fail(function (err) {
+    console.log(err.message);
+    name = "";
+    phone = "";
+    tgName = "";
+  });
 });
